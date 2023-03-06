c37c44be1dce795bc309e360015e8fe7e368d611
anishazahan <anishazahan13@gmail.com>
2023-03-07 00:37:41 +0600

    update

--- src/Components/Home/TopBanner.js
+++ src/Components/Home/TopBanner.js
@@ -13,14 +13,14 @@ import Button from '../Button'
 const TopBanner = () => {
   return (
     <>
-   <div className="banner relative ">
+   <div className="banner relative overflow-x-hidden">
     <Navbar></Navbar>
 
     {/* .........content section......... */}
 
-   <div className="max-w-7xl mx-auto px-16">
+   <div className="max-w-7xl mx-auto ">
 
-   <div className="flex flex-col  text-center mx-auto relative mt-[3rem] ">
+   <div className="px-16 flex flex-col  text-center mx-auto relative mt-[3rem] ">
         <h2 className='text-[#E7AC1F] font-bold'>JOIN THE WORD OF</h2>
         <img className='w-[650px] mx-auto' src={bannerImg} alt="" />
         <p className='font-medium text-xl text-slate-200 tracking-tighter mt-3 '>Be part of puzzle games, duels and olympiads with<br/> more than 20,000 players.</p>
@@ -29,20 +29,28 @@ const TopBanner = () => {
     </div>
     </div>
 
-   <div className="flex justify-between absolute bottom-0 gap-6 mx-auto">
+   <div className="flex justify-between bottom-0 w-full">
 
-   <div className="flex gap-4 relative">
-   <img className=' mb-[-2rem]' src={img1} alt="" />
-   <img className=' mb-[-5rem] ' src={img2} alt="" />
+   <div className="flex gap-4 ">
+        <div className="w-[268px] h-[360px] ">
+        <img className='w-full h-full' src={img1} alt="" />
+        </div>
+        <div className="w-[268px] h-[360px]">
+        <img className='w-full h-full ' src={img2} alt="" />
+        </div>
    </div>
-   
+
    <div className="">
    <img className='' src={img3} alt="" />
    </div>
 
    <div className="flex gap-4">
-   <img className=' mb-[-5rem]' src={img4} alt="" />
-   <img className='  mb-[-2rem]' src={img5} alt="" />
+   <div className="w-[268px] h-[360px] ">
+        <img className='w-full h-full' src={img1} alt="" />
+        </div>
+        <div className="w-[268px] h-[360px]">
+        <img className='w-full h-full ' src={img2} alt="" />
+        </div>
    </div>
    </div>
 
